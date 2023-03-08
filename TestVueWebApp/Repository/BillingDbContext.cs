7015127170932636b3ad08c9e406828375259211
FedotovEM <fedotov.eger@yandex.ru>
2023-03-08 11:52:03 +0300

    Pages translate to russian

--- TestVueWebApp/Repository/BillingDbContext.cs
+++ TestVueWebApp/Repository/BillingDbContext.cs
@@ -38,7 +38,7 @@ namespace TestVueWebApp.Repository
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-F4DDTQQ\\SQLEXPRESS;Database=KURSOVOI_Billing_0315_FEDOTOV_2022;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-F4DDTQQ\\SQLEXPRESS;Database=KURSOVOI_Billing_Correct;Trusted_Connection=True;");
             }
         }
 
